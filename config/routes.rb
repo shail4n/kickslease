11c9028920666bf1d118117275c9e0433eeb778c
Shailan Sheibani <shailansheibani@yahoo.com>
2023-02-28 13:43:05 +0000

    routes and destroty method for booking done

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :shoes do
+    resources :bookings, only: [ :new, :create ]
+  end
+  resources :bookings, only: [ :index, :show, :destroy ]
 end
