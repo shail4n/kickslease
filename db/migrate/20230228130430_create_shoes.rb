a26994fe71130dc255bdd8e1a53dda8131415cd1
Ralu Ugochukwu <ralu2k3@gmail.com>
2023-02-28 14:36:57 +0000

    Updated the schema

--- db/migrate/20230228130430_create_shoes.rb
+++ db/migrate/20230228130430_create_shoes.rb
@@ -1,6 +1,11 @@
 class CreateShoes < ActiveRecord::Migration[7.0]
   def change
     create_table :shoes do |t|
+      t.string :title
+      t.text :description
+      t.integer :sizes
+      t.float :price
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -43,13 +43,25 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_28_131919) do
   end
 
   create_table "bookings", force: :cascade do |t|
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.bigint "shoe_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["shoe_id"], name: "index_bookings_on_shoe_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
   create_table "shoes", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "sizes"
+    t.float "price"
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_shoes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -66,4 +78,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_28_131919) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "bookings", "shoes"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "shoes", "users"
 end
